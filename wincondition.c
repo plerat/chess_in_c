8c9b06ed1442aec342af303596c0e8086a5aceb3
Pierrick Le Ravallec <pierrick.leravallec@gmail.com>
2025-03-11 14:54:03 +0100

    comment on wincondition.c file

--- wincondition.c
+++ wincondition.c
@@ -20,7 +20,7 @@ Point findKing(_Bool color, Piece** board ) {
             }
         }
     }
-    // si le roi n'est pas trouvé, on retourne -1, -1 (hors du board)
+    // If king is not find, return -1, -1 (out of board)
     Point invalid = {-1, -1};
     return invalid;
 }
@@ -41,6 +41,7 @@ _Bool isCaseSafe(_Bool color, LastMove LastMove, Piece** board, int col, int row
 _Bool canKingMove(_Bool player, LastMove LastMove, Piece** board, int col, int row) {
     for (int i = -1; i < 2; i++) {
         for (int j = -1; j < 2; j++) {
+            // to be sure if the check is in the board
             if (col + i > 7 || row + j > 7 || col + i < 0 || row + j < 0) {
                 continue;
             }
@@ -60,6 +61,7 @@ _Bool canKingMove(_Bool player, LastMove LastMove, Piece** board, int col, int r
 _Bool canKnightMove(_Bool player, LastMove LastMove, Piece** board, int col, int row) {
     for (int i= -2; i <= 2; i++) {
         for (int j= -2; j <= 2; j++) {
+            // to be sure if the check is in the board
             if (col + i > 7 || row + j > 7 || col + i < 0 || row + j < 0) {
                 continue;
             }
@@ -75,18 +77,22 @@ _Bool canKnightMove(_Bool player, LastMove LastMove, Piece** board, int col, int
 
 
 _Bool canPieceMove(_Bool player, LastMove LastMove, Piece** board, int col, int row) {
+    // Check the knight separately because we cannot only check if it can move to adjacent squares
     if (getPiece(board,col,row) == WHITE_KNIGHT || getPiece(board,col,row) == BLACK_KNIGHT) {
         return canKnightMove(player, LastMove, board, col, row);
 
     }
+    // Check the king separately because we check if he can move AND if cases around him are safe
     if (getPiece(board,col,row) == BLACK_KING || getPiece(board,col,row) == WHITE_KING) {
         return canKingMove(player, LastMove, board, col, row);
     }
+    // check if all other pieces can move
     for (int i = -1; i < 2; i++) {
         for (int j = -1; j < 2; j++) {
             if (i == 0 && j == 0) {
                 continue;
             }
+            // to be sure if the check is in the board
             if (col + i > 7 || row + j > 7 || col + i < 0 || row + j < 0) {
                 continue;
             }
@@ -98,8 +104,9 @@ _Bool canPieceMove(_Bool player, LastMove LastMove, Piece** board, int col, int 
 }
 
 GameStatus checkWin(_Bool player, Piece** board, LastMove LastMove) {
-    // On cherche le roi ennemi : donc on inverse la valeur de player (on est joueur blanc, on cherche le roi noir)
+    // search enemy king : reverse player value (white player, search black king)
     Point king = findKing(!player, board);
+    // (-1 ; -1) = out of board
     if (king.col == -1 || king.row == -1) {
         return WINNING;
     }
@@ -114,15 +121,18 @@ GameStatus checkWin(_Bool player, Piece** board, LastMove LastMove) {
                 emptyCase++;
                 continue;
             }
-            if (isEnemy(board,  player,i,j) && canPieceMove(!player, LastMove, board,i,j)) {
+            // If enemy can move one piece, he can still play
+            if (isEnemy(board, player,i,j) && canPieceMove(!player, LastMove, board,i,j)) {
                 return ONGOING;
             }
+            // Check if only the 2 kings remains (64 cases by default on chessboard)
             if (emptyCase == 62) {
                 return STALEMATE;
             }
 
         }
     }
+    // Check if the enemy king is "mat"
    if (isCaseSafe(!player, LastMove, board,king.col,king.row)) {
        return STALEMATE;
    }
